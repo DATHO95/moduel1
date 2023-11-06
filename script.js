993a6f9c69df3a710905a7823e2e2de8d4fe389f
DATHO95 <hothanhdatc20@gmail.com>
2023-11-06 21:00:41 +0700

    add

---
+++ script.js
@@ -0,0 +1,14 @@
+let a = 6;
+let b = 10;
+let c = a+b;
+
+
+ console.log("Tổng là: " +c)
+
+// alert("Tổng là: " +c) ô thông báo
+
+// prompt("nhập sô: "); nhập dữ liệu
+
+// confirm("bạn có muốn nhập con số này không"); thông báo xác nhận
+
+// document.write("Tổng là: " +c);
